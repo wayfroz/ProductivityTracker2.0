3558c42e9edd0c6f0b0b17b486402f6fa3e422b6
sakariaabdullahi <sakaria1abdullahi@gmail.com>
2025-04-28 19:01:41 -0500

    database.py tweak

--- backend/database.py
+++ backend/database.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 Base = declarative_base()
-engine = create_engine(os.getenv("DB_URL", "mysql+pymysql://app_user:temp_password@localhost/productivity_db"))
+engine = create_engine(os.getenv("DB_URL", "mysql+pymysql://your_username:your_password@localhost/db_name"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
